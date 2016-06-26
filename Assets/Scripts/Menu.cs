ec42ee8422cb0341e293dbcf761ec39f6797c773
Dhrubajit <dhrubajit1992@gmail.com>
2016-06-27 02:37:11 +0800

    initial commit

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Menu : MonoBehaviour {
+
+	private Text oldScore;
+	// Use this for initialization
+	void Start () {
+		oldScore = GameObject.Find ("score").GetComponent<Text> ();
+		oldScore.text = "Last Score:" + PlayerPrefs.GetInt ("Score");
+
+	}
+
+	public void loadGame(){
+		SceneManager.LoadScene ("GameScene");
+	}
+}
