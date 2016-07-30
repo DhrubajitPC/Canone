8229ec84e706c59007eeeed02196631cce738e9a
Luccan <luccan.ryanata@gmail.com>
2016-07-30 22:42:47 +0800

    Asteroids

---
+++ Assets/Canone/Scripts/AsteroidScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Find("default").transform.Rotate(Vector3.right * 360 * Time.deltaTime);
+    }
+}
