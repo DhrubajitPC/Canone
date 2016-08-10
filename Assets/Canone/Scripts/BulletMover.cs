5386e1073ceecf2999b7bde971a4993c00799cf4
Luccan <luccan.ryanata@gmail.com>
2016-08-11 04:08:52 +0800

    Added explosion effects TODO: immortal bug

--- Assets/Canone/Scripts/BulletMover.cs
+++ Assets/Canone/Scripts/BulletMover.cs
@@ -21,6 +21,7 @@ public class BulletMover : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 
 		Vector3 placement = other.gameObject.transform.position;
+        Quaternion rotation = other.gameObject.transform.rotation;
 		string collidedItem = other.gameObject.name;
 		//		Debug.Log ("Name: " + collidedItem);
 		GameObject ObjParent = other.gameObject.transform.parent.gameObject;
@@ -28,25 +29,25 @@ public class BulletMover : MonoBehaviour {
 
 		Destroy(other.gameObject);
 		print (collidedItem);
+        GameObject explosion_smoke = (GameObject)Instantiate(Resources.Load("prefabs/ExplosionSmoke"), placement, rotation);
+        Destroy(explosion_smoke, 1.0f);
 
-		for (int i = 0; i < 60; i++) {
+        for (int i = 0; i < 60; i++) {
 			if (GameObject.Equals (other.gameObject, ObstacleGenerator.ObsHolder [i])) {
-				if (collidedItem.Contains ("flesh") || collidedItem.Contains ("FleshCube")) {
-					//			Debug.Log ("I hit flesh");
-					ObstacleGenerator.ObsHolder[i] = Instantiate (rubbleTypes [0], placement, Quaternion.identity) as GameObject;
-					ObstacleGenerator.ObsHolder[i].transform.parent = ObjParent.transform;
-					//ObstacleGenerator.ObsHolder[i].transform.position = new Vector3 (rubbleTypes [0].transform.position.x, rubbleTypes [0].transform.position.y, rubbleTypes [0].transform.position.z);
-					//			Debug.Log ("object created: " + rubble.transform.name );
-
-				} else if (collidedItem.Contains ("metal") || collidedItem.Contains ("MetalCube") || collidedItem.Contains ("turret")) {
-					//			Debug.Log ("I hit metal");
-					ObstacleGenerator.ObsHolder[i] = Instantiate (rubbleTypes [1], placement, Quaternion.identity) as GameObject;
-					ObstacleGenerator.ObsHolder[i].transform.parent = ObjParent.transform;
-					//ObstacleGenerator.ObsHolder[i].transform.position = new Vector3 (rubbleTypes [1].transform.position.x, rubbleTypes [1].transform.position.y, rubbleTypes [1].transform.position.z);
-					//print (rubble);
-					//			Debug.Log ("object created: " + rubble.transform.name );
-
+				if (collidedItem.Contains ("flesh") || collidedItem.Contains ("FleshCube") || collidedItem.Contains("turret_flesh")) {
+                    GameObject explosion = (GameObject)Instantiate(Resources.Load("prefabs/ExplosionFlesh"), placement, rotation);
+                    Destroy(explosion, 1.0f);
+                    ObstacleGenerator.ObsHolder[i] = Instantiate (rubbleTypes [0], placement, rotation) as GameObject;
+				} else if (collidedItem.Contains ("metal") || collidedItem.Contains ("MetalCube") || collidedItem.Contains ("turret_metal")) {
+                    GameObject explosion = (GameObject)Instantiate(Resources.Load("prefabs/ExplosionMetal"), placement, rotation);
+                    Destroy(explosion, 1.0f);
+                    ObstacleGenerator.ObsHolder[i] = Instantiate (rubbleTypes [1], placement, rotation) as GameObject;
 				}
+                ObstacleGenerator.ObsHolder[i].transform.parent = ObjParent.transform;
+                if (collidedItem.Contains("Cube"))
+                {
+                    ObstacleGenerator.ObsHolder[i].transform.Translate(0f, 1.5f, 0f);
+                }
 				break;
 			}
 		}
