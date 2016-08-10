828da38a5640bde35d3567060314d542eb3c9449
Dhrubajit <dhrubajit1992@gmail.com>
2016-08-10 23:45:25 +0800

    protos turret moves while attacking

--- Assets/Canone/Scripts/TurretShooter.cs
+++ Assets/Canone/Scripts/TurretShooter.cs
@@ -7,12 +7,14 @@ public class TurretShooter : MonoBehaviour {
 	public GameObject bullet;
 	private GameObject player;
 	bool metalTrack;
-	float bulletCountDownTimer = 1;
+	float bulletCountDownTimer = 0.25f;
 	bool canShoot = true;
+	Vector3 pos;
 
 	// Use this for initialization
 	void Start () {
 		player = GameObject.Find ("Player");
+		pos = transform.position;
 	}
 	
 	// Update is called once per frame
@@ -20,19 +22,18 @@ public class TurretShooter : MonoBehaviour {
 		float dist = (transform.position - player.transform.position).magnitude;
 		Vector3 dir = ((transform.position - player.transform.position) / dist);
 		float angle = Vector3.Angle (player.transform.forward, dir);
-//		print (angle);
-//		gameObject.name == "turret" || gameObject.name == "turret_flesh" && 
+		transform.LookAt (player.transform);
 		if (gameObject.name == "turret" && angle < 30f && dist < 30) {
 			if (canShoot) {
-				GameObject b = Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
+				GameObject b = Instantiate (bullet, bulletSpawn.position, transform.rotation) as GameObject;
 				b.transform.parent = GameObject.Find ("Track").transform;
-				b.transform.LookAt (player.transform);
+//				b.transform.LookAt (player.transform);
 				canShoot = false;
 			}
 			bulletCountDownTimer -= Time.deltaTime;
 			if (bulletCountDownTimer < 0) {
 				canShoot = true;
-				bulletCountDownTimer = 1;
+				bulletCountDownTimer = 0.25f;
 			}
 		} else if (gameObject.name == "turret_flesh" && angle < 135 && dist < 15) {
 			GameObject b = Instantiate (bullet, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
