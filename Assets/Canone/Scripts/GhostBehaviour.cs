dde799e4facd704ccee48432e511d857029db335
linyijuan <linyijuan22@gmail.com>
2016-08-10 23:09:26 +0800

    ghost press s to start mode

---
+++ Assets/Canone/Scripts/GhostBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GhostBehaviour : MonoBehaviour {
+	public float timer;
+	public bool on;
+	public GameObject TimeLeftDisplay;
+
+	public void TurnOnInvisibleMode(){
+		on = true;
+		timer = 10f;
+	}
+	
+	// Update is called once per frame
+	public float UpdateTimeLeft (float deltaTime) {
+		if (on){
+			if (timer <= 0) {
+				TimeLeftDisplay.GetComponent<Text> ().text = "";
+				TimeLeftDisplay.SetActive (false);
+				on = false;
+			} else {
+				TimeLeftDisplay.SetActive (true);
+				timer -= deltaTime;
+				TimeLeftDisplay.GetComponent<Text>().text = "Time Left : "+timer;
+			}
+		}
+		return timer;
+	}
+}
